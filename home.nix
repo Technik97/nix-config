b41c62a350c221421da300efd7465d9ffe06ac20
technik <aniket.97.mane@gmail.com>
2025-02-22 18:18:17 +0530

    Added shell aiases

--- home.nix
+++ home.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+
+  imports = [
+    ./application/shell
+  ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "technik";
